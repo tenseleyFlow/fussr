5001b864ef5adcb5f08dc8ef93f6e1a25df57885
espadonne <espadonne@outlook.com>
2025-12-20 10:10:35 +0000

    Fix link order: use rustc-link-arg for proper library placement

--- build.rs
+++ build.rs
@@ -1,7 +1,7 @@
 fn main() {
     // Explicitly link against libgit2 and libssh2
-    // This works around issues where the -sys crates' build.rs output
-    // isn't being properly processed by cargo
-    println!("cargo:rustc-link-lib=git2");
-    println!("cargo:rustc-link-lib=ssh2");
+    // Use link-arg to append to the END of the linker command line
+    // This ensures the libraries come AFTER the Rust code that needs them
+    println!("cargo:rustc-link-arg=-lgit2");
+    println!("cargo:rustc-link-arg=-lssh2");
 }
